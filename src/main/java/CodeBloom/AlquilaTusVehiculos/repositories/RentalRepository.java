d45fcc1d1b996b17d5a7c80cedaf83586f888c54
androsrivas <androsrivas@outlook.com>
2026-05-11 22:43:21 +0200

    feat: add findByVehicle and overlapping rentals check to RentalRepository

--- src/main/java/CodeBloom/AlquilaTusVehiculos/repositories/RentalRepository.java
+++ src/main/java/CodeBloom/AlquilaTusVehiculos/repositories/RentalRepository.java
@@ -2,11 +2,22 @@ package CodeBloom.AlquilaTusVehiculos.repositories;
 
 import CodeBloom.AlquilaTusVehiculos.models.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, Long> {
     List<Rental> findByEnabledTrue();
+    List<Rental> findByVehicleIdAndEnabledTrue(Long id);
+    @Query("SELECT COUNT(r) > 0 FROM Rental r WHERE r.vehicle.id = :vehicleId "+
+            "AND r.enabled = true " +
+            "AND :startDate < r.estimatedReturnDate " +
+            "AND :endDate > r.startDate")
+    boolean existsOverlappingRentals(@Param("vehicleId") Long vehicleId,
+                                     @Param("startDate")LocalDateTime startDate,
+                                     @Param("endDate") LocalDateTime endDate);
 }
